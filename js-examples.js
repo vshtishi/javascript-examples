bb46bdaa65dc57235199e3496597d4d1b83638df
vshtishi <vjolashtishi@gmail.com>
2020-04-27 11:12:51 +0200

    Reference Data Types

--- js-examples.js
+++ js-examples.js
@@ -1,26 +1,75 @@
-let anObj = {
-    'prop': 'Hello',
-    'prop1': true,
-    'objProp': {
-        'innerProp': 'Inner property value'
+//Arrays
+let arr = [1, 2, 3];
+arr.push(4);
+arr.pop();
+arr.unshift(0);
+arr.forEach(function (item) {
+    console.log('Element ' + item);
+})
+console.log(arr);
+console.log('Accesing Out of bounds index:' + arr[3]);
+console.log(`Array length: ${arr.length}`);
+
+//Functions
+let f = function (name) {
+    console.log('Hello ' + name);
+};
+
+function greet(name) {
+    console.log('Hello ' + name);
+}
+
+let executor = function (fn, name) {
+    fn(name);
+}
+
+greet('Ann');
+greet('Ann', 10);
+f('Ann');
+executor(f, 'JS');
+
+//Objects
+var anObj = {
+    'testProp': true
+};
+
+anObj.aMethod = function () {
+    console.log("Function in object");
+}
+console.log(anObj);
+anObj.aMethod();
+
+var person = {
+    'firstName': 'Jane',
+    'lastName': 'Doe',
+    'getFullName': function () {
+        return this.firstName + " " + this.lastName;
+    },
+    'address': {
+        'street': '123 street',
+        'city': 'x',
+        'state': 'xyz'
+    },
+    'isFromState': function (st) {
+        return this.address.state === st;
     }
 };
 
-console.log(anObj);
+person2 = person;
+person = {};
+console.log(person2.getFullName());
 
-anObj.prop2 = 'value';
+console.log(person2.isFromState('xyz'));
 
-console.log(anObj);
+//Arguments
+var add = function (a, b) {
+    console.log(arguments);
+    let i, sum = 0;
+    for (i = 0; i < arguments.length; i++)
+        sum += arguments[i];
+    return sum;
+}
 
-anObj.prop3 = 10;
-let propertyName = 'prop3';
-console.log(`Number property: ${anObj.prop3}`);
-console.log('Accessing properties using bracket notation: ' + anObj['prop'] + ', ' + anObj[propertyName]);
-console.log('Accessing a non-existent property: ' + anObj.prop4);
-anObj.objProp.newProp='new value';
-console.log('Value of nested object property: ' + anObj.objProp['newProp']);
+console.log(add(10, 20, 30));
 
-//Removing a property
-delete anObj.prop;
-console.log(anObj);
 
