7ad3b3f47cdb16cbc7fb73de9e16c898c4a0e387
vshtishi <vjolashtishi@gmail.com>
2020-05-06 19:24:14 +0200

    JSON (3)

--- js-examples.js
+++ js-examples.js
@@ -8,47 +8,41 @@ request.returnType = 'text';
 request.send();
 
 request.onload = function () {
-   const catString = request.response;
-   const catInfo=JSON.parse(catString);
-   console.log('success');
-   displayCatInfo(catInfo);
+  const catString = request.response;
+  const catInfo = JSON.parse(catString);
+  displayCatInfo(catInfo);
 }
 
-function displayCatInfo(jsonObj){
-       const cats=jsonObj;
-       
+function displayCatInfo(jsonObj) {
+  const cats = jsonObj;
+  let motherInfo = 'The mother cats are called ';
+  let kittenInfo;
+  let maleKitties = 0;
+  let femaleKitties = 0;
+  for (let i = 0; i < cats.length; i++) {
+    if (i == cats.length - 1)
+      motherInfo += ' ' + cats[i].name + '.';
+    else
+      motherInfo += cats[i].name + ', ';
 
-       for(let i=0; i<cats.length; i++){
-        const myArticle = document.createElement('article');
-        const header2 = document.createElement('h2');
-        const para1=document.createElement('p');
-        const para2=document.createElement('p');
-        const para3=document.createElement('p');
-        const header4 = document.createElement('h4');
-        
-        header2.textContent=cats[i].name;
-        para1.textContent='Breed: ' +cats[i].breed;
-        para2.textContent='Color: ' +cats[i].color;
-        header4.textContent='Kittens: ';
+    const kitties = cats[i].kittens;
 
-        const kitties=cats[i].kittens;
-        for(let j=0; j<kitties.length; j++){
-          const aList = document.createElement('ul');
-          const listItem1 = document.createElement('li');
-          const listItem2 = document.createElement('li');
-          listItem1.textContent = 'Name: ' +kitties[j].name;
-          listItem2.textContent = 'Gender:' +kitties[j].gender;
-          aList.appendChild(listItem1);
-          aList.appendChild(listItem2);
-          para3.appendChild(aList);
-        }
+    for (let j = 0; j < kitties.length; j++) {
+      if (kitties[j].gender == 'f')
+        femaleKitties++;
+      else
+        maleKitties++;
 
-        myArticle.appendChild(header2);
-        myArticle.appendChild(para1);
-        myArticle.appendChild(para2);
-        myArticle.appendChild(header4);
-        myArticle.appendChild(para3);
-    
-        section.appendChild(myArticle);
-       }
+    }
+
+  }
+  totalKitties = maleKitties + femaleKitties;
+  kittyInfo = 'There are ' + totalKitties + ' kittens.' + maleKitties + ' male kittens and ' + femaleKitties + ' female kittens';
+  const para1 = document.createElement('p');
+  para1.style.color='purple';
+  const para2 = document.createElement('p');
+  para1.textContent = motherInfo;
+  para2.textContent = kittyInfo;
+  section.appendChild(para1);
+  section.appendChild(para2);
 }
