5fcd4e9cffb0ed526838830f2b96a3076de56bf7
vshtishi <vjolashtishi@gmail.com>
2020-05-06 17:31:13 +0200

    JSON (2)

--- js-examples.js
+++ js-examples.js
@@ -1,64 +1,54 @@
 const header = document.querySelector('header');
 const section = document.querySelector('section');
-let requestURL = 'https://raw.githubusercontent.com/vshtishi/javascript-examples/master/employees.json';
+let requestURL = 'https://raw.githubusercontent.com/mdn/learning-area/master/javascript/oojs/tasks/json/sample.json';
 
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
-//request.responseType = 'json';
-request.responseType='text';
+request.returnType = 'text';
 request.send();
 
-request.onload=function(){
-    const companyEmployeesText=request.response;
-    const companyEmployees=JSON.parse(companyEmployeesText);
-    populateHeader(companyEmployees);
-    showEmployees(companyEmployees);
+request.onload = function () {
+   const catString = request.response;
+   const catInfo=JSON.parse(catString);
+   console.log('success');
+   displayCatInfo(catInfo);
 }
 
-function populateHeader(jsonObj) {
-    const myH1 = document.createElement('h1');
-    myH1.textContent = jsonObj['company'];
-    header.appendChild(myH1);
-  
-    const myPara = document.createElement('p');
-    myPara.textContent = 'City: ' + jsonObj['city'] + ' // Founded: ' + jsonObj['founded'];
-    header.appendChild(myPara);
-  }
+function displayCatInfo(jsonObj){
+       const cats=jsonObj;
+       
 
-  function showEmployees(jsonObj) {
-    const employees = jsonObj['employees'];
+       for(let i=0; i<cats.length; i++){
+        const myArticle = document.createElement('article');
+        const header2 = document.createElement('h2');
+        const para1=document.createElement('p');
+        const para2=document.createElement('p');
+        const para3=document.createElement('p');
+        const header4 = document.createElement('h4');
         
-    for (let i = 0; i < employees.length; i++) {
-      const myArticle = document.createElement('article');
-      const myH2 = document.createElement('h2');
-      const myPara1 = document.createElement('p');
-      const myPara2 = document.createElement('p');
-      const myPara3 = document.createElement('p');
-      const myList = document.createElement('ul');
-  
-      myH2.textContent = employees[i].name;
-      myPara1.textContent = 'Position: ' + employees[i].position;
-      myPara2.textContent = 'Age: ' + employees[i].age;
-      myPara3.textContent = 'Technologies:';
-          
-      const techs = employees[i].technologies;
-      for (let j = 0; j < techs.length; j++) {
-        const listItem = document.createElement('li');
-        listItem.textContent = techs[j];
-        myList.appendChild(listItem);
-      }
-  
-      myArticle.appendChild(myH2);
-      myArticle.appendChild(myPara1);
-      myArticle.appendChild(myPara2);
-      myArticle.appendChild(myPara3);
-      myArticle.appendChild(myList);
-  
-      section.appendChild(myArticle);
-    }
-  }
+        header2.textContent=cats[i].name;
+        para1.textContent='Breed: ' +cats[i].breed;
+        para2.textContent='Color: ' +cats[i].color;
+        header4.textContent='Kittens: ';
 
-let myJSON = { "name": "Chris", "age": "38" };
-myJSON
-let myString = JSON.stringify(myJSON);
-console.log(myString);
+        const kitties=cats[i].kittens;
+        for(let j=0; j<kitties.length; j++){
+          const aList = document.createElement('ul');
+          const listItem1 = document.createElement('li');
+          const listItem2 = document.createElement('li');
+          listItem1.textContent = 'Name: ' +kitties[j].name;
+          listItem2.textContent = 'Gender:' +kitties[j].gender;
+          aList.appendChild(listItem1);
+          aList.appendChild(listItem2);
+          para3.appendChild(aList);
+        }
+
+        myArticle.appendChild(header2);
+        myArticle.appendChild(para1);
+        myArticle.appendChild(para2);
+        myArticle.appendChild(header4);
+        myArticle.appendChild(para3);
+    
+        section.appendChild(myArticle);
+       }
+}
