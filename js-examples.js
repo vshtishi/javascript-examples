40e1cc0399de0c1638b577e9a39e4fbbc748a6a7
vshtishi <vjolashtishi@gmail.com>
2020-05-09 17:40:39 +0200

    Bouncing balls example

--- js-examples.js
+++ js-examples.js
@@ -1,48 +1,202 @@
-const header = document.querySelector('header');
-const section = document.querySelector('section');
-let requestURL = 'https://raw.githubusercontent.com/mdn/learning-area/master/javascript/oojs/tasks/json/sample.json';
+// setup canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-let request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.returnType = 'text';
-request.send();
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
-request.onload = function () {
-  const catString = request.response;
-  const catInfo = JSON.parse(catString);
-  displayCatInfo(catInfo);
+const para1 = document.querySelector('p');
+let count = 0;
+
+function updateCount(count){
+  para1.textContent='Ball count:' + count;
 }
 
-function displayCatInfo(jsonObj) {
-  const cats = jsonObj;
-  let motherInfo = 'The mother cats are called ';
-  let kittenInfo;
-  let maleKitties = 0;
-  let femaleKitties = 0;
-  for (let i = 0; i < cats.length; i++) {
-    if (i == cats.length - 1)
-      motherInfo += ' ' + cats[i].name + '.';
-    else
-      motherInfo += cats[i].name + ', ';
+// function to generate random number
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min)) + min;
+  return num;
+}
 
-    const kitties = cats[i].kittens;
+function Shape(x, y, velX, velY, exists) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.exists = exists;
+}
 
-    for (let j = 0; j < kitties.length; j++) {
-      if (kitties[j].gender == 'f')
-        femaleKitties++;
-      else
-        maleKitties++;
-
-    }
-
+class Ball extends Shape {
+  constructor(x, y, velX, velY, exists, color, size) {
+    super(x, y, velX, velY, exists);
+    this.color = color;
+    this.size = size;
   }
-  totalKitties = maleKitties + femaleKitties;
-  kittyInfo = 'There are ' + totalKitties + ' kittens.' + maleKitties + ' male kittens and ' + femaleKitties + ' female kittens';
-  const para1 = document.createElement('p');
-  para1.style.color='purple';
-  const para2 = document.createElement('p');
-  para1.textContent = motherInfo;
-  para2.textContent = kittyInfo;
-  section.appendChild(para1);
-  section.appendChild(para2);
 }
+
+function EvilCircle(x, y, exists, color, size) {
+
+  Shape.call(this,x, y, 20, 20, exists);
+  this.color ='rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
+  this.size = 10;
+}
+EvilCircle.prototype = Object.create(Shape.prototype);
+Object.defineProperty(EvilCircle.prototype, 'constructor', {
+  value: EvilCircle,
+  enumerable: false, // so that it does not appear in 'for in' loop
+  writable: true
+});
+
+
+EvilCircle.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.strokeStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.stroke();
+}
+
+
+EvilCircle.prototype.checkBounds = function () {
+  if ((this.x + this.size) >= width) {
+    this.x -=(this.size);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.x +=(this.size);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.x -=(this.size);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.x +=(this.size);
+  }
+}
+
+EvilCircle.prototype.setControls=function(){
+  let _this = this;
+window.onkeydown = function(e) {
+    if (e.key === 'a') {
+      _this.x -= _this.velX;
+    } else if (e.key === 'd') {
+      _this.x += _this.velX;
+    } else if (e.key === 'w') {
+      _this.y -= _this.velY;
+    } else if (e.key === 's') {
+      _this.y += _this.velY;
+    }
+  }
+}
+
+EvilCircle.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (balls[j].exists) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].exists=false;
+        count--;
+        updateCount(count);
+      }
+    }
+  }
+}
+
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+Ball.prototype.collisionDetect = function () {
+  for (let j = 0; j < balls.length; j++) {
+    if (!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+      }
+    }
+  }
+}
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 20);
+  let ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    true,
+    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+    size
+  );
+
+  balls.push(ball);
+  count++;
+  updateCount(count);
+  
+}
+let evilCircle=new EvilCircle(
+  random(0, width),
+    random(0 , height),
+  true,
+  'rgb(255 , 255 ,255)'
+  );
+  evilCircle.setControls();
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+  
+
+  for (let i = 0; i < balls.length; i++) {
+    evilCircle.draw();
+    evilCircle.checkBounds();
+    evilCircle.collisionDetect();
+    if(balls[i].exists){
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+    }
+   
+  
+  }
+  
+  requestAnimationFrame(loop);
+}
+
+loop();
