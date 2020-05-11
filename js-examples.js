198bfc4c63d177c0bcb7888fd75966cf2127ae06
vshtishi <vjolashtishi@gmail.com>
2020-05-11 15:50:55 +0200

    Stopwatch example

--- js-examples.js
+++ js-examples.js
@@ -1,202 +1,55 @@
-// setup canvas
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
-
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-
-const para1 = document.querySelector('p');
-let count = 0;
-
-function updateCount(count){
-  para1.textContent='Ball count:' + count;
-}
-
-// function to generate random number
-function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min)) + min;
-  return num;
-}
-
-function Shape(x, y, velX, velY, exists) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.exists = exists;
-}
-
-class Ball extends Shape {
-  constructor(x, y, velX, velY, exists, color, size) {
-    super(x, y, velX, velY, exists);
-    this.color = color;
-    this.size = size;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+document.getElementById('demo').textContent = '00 : 00 : 00';
+function start() {
+  if (seconds < 59)
+    seconds++;
+  else {
+    seconds = 0;
+    if (minutes < 59)
+      minutes++;
+    else {
+      minutes = 0;
+      hours++;
+    }
   }
+  let showSeconds = seconds < 10 ? `0${seconds}`.slice(-2) : seconds;
+  let showMinutes = minutes < 10 ? `0${minutes}`.slice(-2) : minutes;
+  let showHours = hours < 10 ? `0${hours}`.slice(-2) : hours;
+  document.getElementById('demo').textContent = `${showHours} : ${showMinutes} : ${showSeconds}`;
+
+
 }
 
-function EvilCircle(x, y, exists, color, size) {
-
-  Shape.call(this,x, y, 20, 20, exists);
-  this.color ='rgb(' + 255 + ',' + 255 + ',' + 255 + ')';
-  this.size = 10;
+function reset() {
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  document.getElementById('demo').textContent = '00 : 00 : 00';
 }
-EvilCircle.prototype = Object.create(Shape.prototype);
-Object.defineProperty(EvilCircle.prototype, 'constructor', {
-  value: EvilCircle,
-  enumerable: false, // so that it does not appear in 'for in' loop
-  writable: true
+
+
+let startStopwatch;
+let startButton = document.getElementById('start');
+let stopButton = document.getElementById('stop');
+let resetButton = document.getElementById('reset');
+
+startButton.addEventListener('click', () => {
+  startStopwatch = setInterval(start, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
 });
 
+stopButton.addEventListener('click', () => {
+  clearInterval(startStopwatch);
+  stopButton.disabled = true;
+  startButton.disabled = false;
 
-EvilCircle.prototype.draw = function () {
-  ctx.beginPath();
-  ctx.lineWidth = 3;
-  ctx.strokeStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.stroke();
-}
+});
 
-
-EvilCircle.prototype.checkBounds = function () {
-  if ((this.x + this.size) >= width) {
-    this.x -=(this.size);
-  }
-
-  if ((this.x - this.size) <= 0) {
-    this.x +=(this.size);
-  }
-
-  if ((this.y + this.size) >= height) {
-    this.x -=(this.size);
-  }
-
-  if ((this.y - this.size) <= 0) {
-    this.x +=(this.size);
-  }
-}
-
-EvilCircle.prototype.setControls=function(){
-  let _this = this;
-window.onkeydown = function(e) {
-    if (e.key === 'a') {
-      _this.x -= _this.velX;
-    } else if (e.key === 'd') {
-      _this.x += _this.velX;
-    } else if (e.key === 'w') {
-      _this.y -= _this.velY;
-    } else if (e.key === 's') {
-      _this.y += _this.velY;
-    }
-  }
-}
-
-EvilCircle.prototype.collisionDetect = function () {
-  for (let j = 0; j < balls.length; j++) {
-    if (balls[j].exists) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].exists=false;
-        count--;
-        updateCount(count);
-      }
-    }
-  }
-}
-
-
-Ball.prototype.draw = function () {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-}
-
-Ball.prototype.update = function () {
-  if ((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-
-  if ((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if ((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-
-  if ((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-}
-
-Ball.prototype.collisionDetect = function () {
-  for (let j = 0; j < balls.length; j++) {
-    if (!(this === balls[j])) {
-      const dx = this.x - balls[j].x;
-      const dy = this.y - balls[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balls[j].size) {
-        balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
-      }
-    }
-  }
-}
-
-let balls = [];
-
-while (balls.length < 25) {
-  let size = random(10, 20);
-  let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
-    random(0 + size, width - size),
-    random(0 + size, height - size),
-    random(-7, 7),
-    random(-7, 7),
-    true,
-    'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
-    size
-  );
-
-  balls.push(ball);
-  count++;
-  updateCount(count);
-  
-}
-let evilCircle=new EvilCircle(
-  random(0, width),
-    random(0 , height),
-  true,
-  'rgb(255 , 255 ,255)'
-  );
-  evilCircle.setControls();
-
-function loop() {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-  ctx.fillRect(0, 0, width, height);
-  
-
-  for (let i = 0; i < balls.length; i++) {
-    evilCircle.draw();
-    evilCircle.checkBounds();
-    evilCircle.collisionDetect();
-    if(balls[i].exists){
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
-    }
-   
-  
-  }
-  
-  requestAnimationFrame(loop);
-}
-
-loop();
+resetButton.addEventListener('click', () => {
+  reset();
+  startButton.disabled = false;
+  stopButton.disabled = false;
+});
